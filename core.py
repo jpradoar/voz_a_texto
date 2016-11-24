4a66541854c5ec33df35fe9a0b0ff86b83195e59
Jonathan <jpradoar@gmail.com>
2016-11-24 00:06:24 -0300

    Update core.py

--- core.py
+++ core.py
@@ -6,6 +6,7 @@ import time
 import subprocess
 
 
+
 """
 comando = ""
 i = 0
@@ -15,10 +16,29 @@ for arg in sys.argv:
  i = i + 1
 comando = comando[0:len(comando)-1]
 """
+
+
 os.system("> socket")
 while True:
 	if os.stat("socket").st_size != 0:
-		os.system("cat socket | festival --tts --language spanish")
-		time.sleep(0.5)		
-		os.system("> socket")
+		if "Prueba" in open('socket').read():
+    			os.system("cat socket | festival --tts --language spanish")
+    			time.sleep(0.5)
+    			os.system("> socket")
+    	if "Hola" in open('socket').read():
+    			os.system("cat socket | festival --tts --language spanish")
+    			time.sleep(0.5)
+    			os.system("> socket")
+    	if "Prender luces" in open('socket').read():
+    			os.system("cat socket | festival --tts --language spanish")
+    			time.sleep(0.5)
+    			os.system("> socket")
+    	if "IP" in open('socket').read():
+    			os.system("ifconfig wlan0 | grep inet | grep -v inet6 | awk {'print $2'} | cut -d':' -f2 | festival --tts --language spanish")
+    			time.sleep(0.5)
+    			os.system("> socket")
+
+	else:
+		time.sleep(1)	
+		print time.strftime("%Y-%m-%d %H:%M:%S")
 
